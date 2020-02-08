8faff2f6314be95b587c5a6f7d4ba2f25ef2aec6
Abhinash Bihari <abhinashbihari1998@gmail.com>
2020-02-08 11:32:06 -0800

    first commit

---
+++ textutils/urls.py
@@ -0,0 +1,36 @@
+"""textutils URL Configuration
+{% extends 'base.html' %}
+{% block content %}
+
+
+<h1>Hello {{name}}!!!</h1>
+<form text=""
+
+{% endblock %}
+
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+
+
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('analyze', views.analyze, name='analyze')
+]
